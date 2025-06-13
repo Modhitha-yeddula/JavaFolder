b99c6b3fb9c0bf9ac7ace839326d264858b0a632
Modhitha yeddula <modhithayeddula@gmail.com>
2025-06-13 11:54:01 +0530

    first commit

---
+++ OOPS/Demo.java
@@ -0,0 +1,16 @@
+ class Account {
+    public String toString(){
+    String name="ranjini kanth";
+ }
+}
+public class Demo{
+    public static void main(String[] args){
+        Account a1=new Account();
+        Account a2=new Account();
+        Account a3=new Account();
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+        System.out.println(a3.toString());
+ 
+    }
+}
